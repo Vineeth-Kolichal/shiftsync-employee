f7f95bb93d8d6ce0b12de398e007f20f6c23a1db
Vineeth-Kolichal <vineethchandran5898@gmail.com>
2023-07-08 19:52:39 +0530

    salary details screen is connected with backedn

--- lib/presentation/pages/home_screen_pages/salary_details/salary_details_page.dart
+++ lib/presentation/pages/home_screen_pages/salary_details/salary_details_page.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:shiftsync/bussiness_logic/blocs/salary_details_page/salary_details_page_bloc.dart';
 import 'package:shiftsync/presentation/pages/home_screen_pages/salary_details/widgets/pay_details_row_widget.dart';
 import 'package:shiftsync/presentation/widgets/bold_title_text.dart';
 import 'package:shiftsync/util/colors/background_colors.dart';
@@ -12,85 +14,128 @@ class SalaryDetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      context.read<SalaryDetailsPageBloc>().add(GetDetails());
+    });
+    Size size = MediaQuery.of(context).size;
     return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            BoldTitleText(title: 'Pay detials'),
-            kDividerTwo,
-            PayDetilsRowWidget(
-              leadingText: 'Grade:',
-              text: 'A level',
-              fontColor: customPrimaryColor,
-              prefix: '',
-            ),
-            PayDetilsRowWidget(
-              leadingText: 'Pay type:',
-              text: 'Salary',
-              prefix: '',
-            ),
-            PayDetilsRowWidget(
-              leadingText: 'Pay frequency:',
-              text: 'Monthly',
-              prefix: '',
-            ),
-            PayDetilsRowWidget(
-              leadingText: 'Base salary',
-              text: '18000',
-            ),
-            kDividerTwo,
-            PayDetilsRowWidget(
-              leadingText: 'Dearness Allowance',
-              text: '800',
-            ),
-            PayDetilsRowWidget(
-              leadingText: 'Special Allowance',
-              text: '100',
-            ),
-            PayDetilsRowWidget(
-              leadingText: 'Medical Allowance',
-              text: '200',
-            ),
-            PayDetilsRowWidget(
-              leadingText: 'Over time',
-              text: '300',
-            ),
-            PayDetilsRowWidget(
-              leadingText: 'Gross Salary',
-              leadingColor: Colors.black,
-              fontSize: 18,
-              fontColor: Colors.green,
-              leadingFontSize: 19,
-              text: '19300',
-            ),
-            kDividerTwo,
-            PayDetilsRowWidget(leadingText: 'Professional Tax', text: '200'),
-            PayDetilsRowWidget(leadingText: 'ProvidentFund', text: '100'),
-            PayDetilsRowWidget(
-              leadingText: 'Total Deductions',
-              leadingColor: Colors.black,
-              fontSize: 18,
-              fontColor: Colors.red,
-              leadingFontSize: 19,
-              text: '300',
-            ),
-            kDividerTwo,
-            PayDetilsRowWidget(
-              leadingText: 'Net Salary',
-              leadingColor: Colors.black,
-              fontSize: 21,
-              fontColor: Colors.green,
-              leadingFontSize: 22,
-              text: '19000',
-            ),
-            TextButton.icon(
-                onPressed: () {},
-                icon: Icon(Iconsax.document_download),
-                label: Text('Download salary slip'))
-          ],
-        ),
+      child: BlocBuilder<SalaryDetailsPageBloc, SalaryDetailsPageState>(
+        builder: (context, state) {
+          if (state.isLoading) {
+            return SizedBox(
+              height: size.height,
+              width: size.width,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else if (state.salaryDetailsModel?.status == 200) {
+            return SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BoldTitleText(title: 'Pay detials'),
+                  kDividerTwo,
+                  PayDetilsRowWidget(
+                    leadingText: 'Grade:',
+                    text: state.salaryDetailsModel?.salaryDetails?.grade ??
+                        'Grade',
+                    fontColor: customPrimaryColor,
+                    prefix: '',
+                  ),
+                  PayDetilsRowWidget(
+                    leadingText: 'Pay type:',
+                    text: 'Salary',
+                    prefix: '',
+                  ),
+                  PayDetilsRowWidget(
+                    leadingText: 'Pay frequency:',
+                    text: 'Monthly',
+                    prefix: '',
+                  ),
+                  PayDetilsRowWidget(
+                    leadingText: 'Base salary',
+                    text:
+                        '${state.salaryDetailsModel?.salaryDetails?.basesalary}',
+                  ),
+                  kDividerTwo,
+                  PayDetilsRowWidget(
+                    leadingText: 'Dearness Allowance',
+                    text:
+                        '${state.salaryDetailsModel?.salaryDetails?.dallowance}',
+                  ),
+                  PayDetilsRowWidget(
+                    leadingText: 'Special Allowance',
+                    text:
+                        '${state.salaryDetailsModel?.salaryDetails?.dallowance}',
+                  ),
+                  PayDetilsRowWidget(
+                    leadingText: 'Medical Allowance',
+                    text:
+                        '${state.salaryDetailsModel?.salaryDetails?.mallowance}',
+                  ),
+                  PayDetilsRowWidget(
+                    leadingText: 'Over time',
+                    text:
+                        '${state.salaryDetailsModel?.salaryDetails?.overtime}',
+                  ),
+                  PayDetilsRowWidget(
+                    leadingText: 'Gross Salary',
+                    leadingColor: Colors.black,
+                    fontSize: 18,
+                    fontColor: Colors.green,
+                    leadingFontSize: 19,
+                    text:
+                        '${state.salaryDetailsModel?.salaryDetails?.grosssalary}',
+                  ),
+                  kDividerTwo,
+                  PayDetilsRowWidget(
+                      fontColor: Colors.red,
+                      leadingText: 'Professional Tax',
+                      text: '${state.salaryDetailsModel?.salaryDetails?.tax}'),
+                  PayDetilsRowWidget(
+                    leadingText: 'ProvidentFund',
+                    text:
+                        '${state.salaryDetailsModel?.salaryDetails?.provident}',
+                    fontColor: Colors.red,
+                  ),
+                  // PayDetilsRowWidget(
+                  //   leadingText: 'Total Deductions',
+                  //   leadingColor: Colors.black,
+                  //   fontSize: 18,
+                  //   fontColor: Colors.red,
+                  //   leadingFontSize: 19,
+                  //   text: '${state.salaryDetailsModel?.salaryDetails!.tax!+state.salaryDetailsModel?.salaryDetails!.provident}',
+                  // ),
+                  kDividerTwo,
+                  PayDetilsRowWidget(
+                    leadingText: 'Net Salary',
+                    leadingColor: Colors.black,
+                    fontSize: 21,
+                    fontColor: Colors.green,
+                    leadingFontSize: 22,
+                    text:
+                        '${state.salaryDetailsModel?.salaryDetails?.netsalary}',
+                  ),
+                  TextButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Iconsax.document_download),
+                      label: Text('Download salary slip'))
+                ],
+              ),
+            );
+          } else {
+            return SizedBox(
+              height: size.height,
+              width: size.width,
+              child: Center(
+                child: Text(state.salaryDetailsModel!.message ??
+                    'Something Went wrong'),
+              ),
+            );
+          }
+        },
       ),
     );
   }
